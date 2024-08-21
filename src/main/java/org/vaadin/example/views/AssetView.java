147d1414ba4c2e351f8d3b05207e22ce4e94b69a
Shubh250 <sbha878@aucklanduni.ac.nz>
2024-08-21 17:53:00 +1200

    Creating the asset view constructor

--- src/main/java/org/vaadin/example/views/AssetView.java
+++ src/main/java/org/vaadin/example/views/AssetView.java
@@ -9,11 +9,24 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import org.vaadin.example.MainLayout;
 import org.vaadin.example.model.Budget;
+import org.vaadin.example.service.AssetService;
 import org.vaadin.example.service.BudgetService;
 import org.vaadin.example.service.SessionService;
 import org.vaadin.example.service.UserService;
 
 @Route(value = "asset", layout = MainLayout.class)
 public class AssetView extends VerticalLayout {
+
+    private final AssetService assetService;
+    private final SessionService sessionService;
+    private final UserService userService;
     
+
+    public AssetView(AssetService assetService, SessionService sessionService, UserService userService){
+        this.assetService = assetService;
+        this.sessionService = sessionService;
+        this.userService = userService;
+
+    
+    }
 }
